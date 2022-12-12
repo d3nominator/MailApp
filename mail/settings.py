55ee349bbb7d10ea7a3115d0aa353d22e00662a7
d3nominator <rishabh.pandey7681@gmail.com>
2022-12-13 00:19:12 +0530

    all pages added

--- mail/settings.py
+++ mail/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2bbih3t)rdje#e9z)$=bh(gmqkttkx&e$-guo(+z=qk3fh=m#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True;
 
 ALLOWED_HOSTS = []
 
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "mailapp.User"
+
